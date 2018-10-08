84abba3e8e0e30598f464992b4035a355412959f
Yuvaraj <shivarajnaidu@gmail.com>
2018-10-08 23:32:15 +0530

    Login Signup Services Integrated

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,13 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authServ: AuthService
+  ) { }
 
   ngOnInit() {
   }
 
-  handleSubmit(form) {
-    console.log(form.value);
+  async handleSubmit(form) {
+    const data = form.value;
+    this.authServ.signup(data);
   }
 
 }
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -19,23 +19,30 @@ export class AuthService {
     private settingServ: SettingsService
   ) { }
 
+  private promisefyPostReq(url, data) {
+    return new Promise((resolve, reject) => {
+      this.http.post(url, data)
+        .subscribe(resolve, reject);
+    });
+  }
+
   login(data) {
     const url = this.API_URLs.login;
-    return this.http.post(url, data);
+    return this.promisefyPostReq(url, data);
   }
 
   signup(data) {
     const url = this.API_URLs.signup;
-    return this.http.post(url, data);
+    return this.promisefyPostReq(url, data);
   }
 
   forgotPassword(data) {
     const url = this.API_URLs.forgot;
-    return this.http.post(url, data);
+    return this.promisefyPostReq(url, data);
   }
 
   resetPassword(data) {
     const url = this.API_URLs.reset;
-    return this.http.post(url, data);
+    return this.promisefyPostReq(url, data);
   }
 }
