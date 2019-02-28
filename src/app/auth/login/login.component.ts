692ef5df989e624f5a335c3ffb5ad380f708b1b8
nagarajnaidu <nagarajnaidu921@gmail.com>
2019-02-28 13:32:26 +0530

    route changing added after successful login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth/auth.service';
 import { TokenService } from '../../services/token/token.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -22,6 +23,7 @@ export class LoginComponent implements OnInit {
     const loginResponse: any = await this.authServ.login(data);
     this.tokenServ.token = loginResponse.token;
     this.authServ.subject.next(true);
+    this.router.navigate(['/']);
   }
 
 }
