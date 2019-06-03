b36266ee311873cbabd47c3be9c81b6510875f7d
Yuvaraj <shivarajnaidu@gmail.com>
2019-06-03 23:50:27 +0530

    v7 to 8 upgrade

--- src/app/mat-components/mat-components.module.ts
+++ src/app/mat-components/mat-components.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 
 
-import {
-  MatButtonModule,
-  MatCheckboxModule,
-  MatToolbarModule,
-  MatInputModule,
-  MatSidenavModule,
-  MatListModule,
-  MatIconModule,
-  MatTableModule,
-  MatSortModule,
-  MatPaginatorModule,
-  MatSelectModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 const matComponents = [
   MatButtonModule,
