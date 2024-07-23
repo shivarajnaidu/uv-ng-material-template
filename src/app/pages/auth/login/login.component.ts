f18f0f90fdd8efa899bf2dc679afe0bde4c82333
Yuvaraj V <yuvaraj.vemula@traceone.com>
2024-07-23 13:22:15 +0530

    added login page

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatError, MatFormField, MatHint, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormField,
+    MatHint,
+    MatLabel,
+    MatInput,
+    MatError,
+    MatButton,
+    RouterLink,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
+  #fb = inject(FormBuilder);
+  passwordHintText = 'Eg @#!%787@';
+  loginForm = this.#fb.group({
+    email: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
 
 }
