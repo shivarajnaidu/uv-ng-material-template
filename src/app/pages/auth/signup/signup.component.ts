75d40ae344afb29fc7f260a53cde1e07a6bf37d1
Yuvaraj V <yuvaraj.vemula@traceone.com>
2024-07-23 13:26:47 +0530

    signup page added

--- src/app/pages/auth/signup/signup.component.ts
+++ src/app/pages/auth/signup/signup.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatFormField, MatHint, MatLabel, MatError } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormField,
+    MatHint,
+    MatLabel,
+    MatInput,
+    MatError,
+    MatButton,
+    MatIcon,
+    RouterLink,
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
-
+  #fb = inject(FormBuilder);
+  signupForm = this.#fb.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    mobile: ['', Validators.required],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    cPassword: ['', [Validators.required, Validators.minLength(6)]],
+  });
 }
